50a1ba7ceb4762e7a5eb0bf8b45336f74d9e1dd2
Umut SAYDAM <umutsaydam24@gmail.com>
2024-02-20 15:12:49 +0300

    alarm scheduler fixed

--- app/src/main/java/com/umutsaydam/alarmapp/helpers/AlarmSchedule.kt
+++ app/src/main/java/com/umutsaydam/alarmapp/helpers/AlarmSchedule.kt
@@ -6,15 +6,12 @@ class AlarmSchedule : IAlarmSchedule {
     private val calendar: Calendar = Calendar.getInstance()
 
     override fun alarmReschedule(alarmTime: Long, alarmRepeat: List<Int>): Long {
-        calendar.timeInMillis = alarmTime
-        var today = calendar[Calendar.DAY_OF_WEEK]
+        val today = calendar[Calendar.DAY_OF_WEEK]
 
-        return if (alarmRepeat.contains(today)) {
+        return if (alarmRepeat.contains(today) && calendar.timeInMillis < alarmTime) {
             alarmTime
         } else {
-            if (today == 7) {
-                today = 1
-            }
+            calendar.timeInMillis = alarmTime
             val firstDay = alarmRepeat.find { it > today }
             if (firstDay == null) {
                 alarmTime + ((7 - today + alarmRepeat[0]) * 24 * 60 * 60 * 1000)
