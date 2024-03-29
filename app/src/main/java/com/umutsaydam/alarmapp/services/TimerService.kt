298a361ef91fde9e0093825575b16efb57920ffa
Umut SAYDAM <umutsaydam24@gmail.com>
2024-03-29 10:03:26 +0300

    count down timer service is working perfectly, Added TimerFragment viewer and receiver

---
+++ app/src/main/java/com/umutsaydam/alarmapp/services/TimerService.kt
@@ -0,0 +1,99 @@
+package com.umutsaydam.alarmapp.services
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.CountDownTimer
+import android.os.IBinder
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.umutsaydam.alarmapp.R
+
+class TimerService : Service() {
+    private var countDownTimer: CountDownTimer? = null
+    private var isTimerRunning = false
+    private var service: NotificationManager? = null
+    private var notificationBuilder: NotificationCompat.Builder? = null
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val hour = intent?.getIntExtra("hour", 0) ?: 0
+        val minute = intent?.getIntExtra("minute", 0) ?: 0
+        val countdownTime = (hour * 60 * 60) + (minute * 60)
+        startForegroundService(countdownTime)
+        return START_STICKY
+    }
+
+    private fun startForegroundService(countdownTime: Int) {
+        if (!isTimerRunning) {
+            var notification = createNotification()
+            startForeground(1, notification)
+            countDownTimer = object : CountDownTimer((countdownTime * 1000).toLong(), 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    val secondsLeft = (millisUntilFinished / 1000).toInt()
+                    Log.d("R/T", secondsLeft.toString())
+
+                    val intent = Intent("TIMER_TICK")
+                    intent.putExtra("timeLeftFormatted", formatTime(secondsLeft))
+                    intent.putExtra("timeLeft", formatTime(secondsLeft))
+                    sendBroadcast(intent)
+
+                    notificationBuilder?.let {
+                        it.setContentText(formatTime(secondsLeft))
+                        notification = it.build()
+                        service!!.notify(1, notification)
+                    }
+                }
+
+                private fun formatTime(secondsLeft: Int): String {
+                    val hours = secondsLeft / 3600
+                    val minutes = (secondsLeft % 3600) / 60
+                    val seconds = secondsLeft % 60
+                    return String.format("%02d:%02d:%02d", hours, minutes, seconds)
+                }
+
+                override fun onFinish() {
+                    isTimerRunning = false
+                    stopSelf()
+                }
+
+            }
+            countDownTimer?.start()
+            isTimerRunning = true
+        }
+    }
+
+    private fun createNotification(): Notification {
+        val channelId =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel("my_service", "My Background Service")
+            } else {
+                ""
+            }
+        notificationBuilder = NotificationCompat.Builder(applicationContext, channelId)
+        return notificationBuilder!!.setOngoing(true)
+            .setContentTitle("Timer Service")
+            .setOnlyAlertOnce(true)
+            .setSmallIcon(R.drawable.ic_timer)
+            .setContentText("test")
+            .setPriority(NotificationCompat.PRIORITY_MIN)
+            .setCategory(NotificationCompat.CATEGORY_SERVICE)
+            .build()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        val channel =
+            NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_NONE)
+        service = getSystemService(NotificationManager::class.java) as NotificationManager
+        service!!.createNotificationChannel(channel)
+        return channelId
+    }
+}
--- app/src/main/java/com/umutsaydam/alarmapp/ui/fragments/TimerFragment.kt
+++ app/src/main/java/com/umutsaydam/alarmapp/ui/fragments/TimerFragment.kt
@@ -1,33 +1,28 @@
 package com.umutsaydam.alarmapp.ui.fragments
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
-import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.umutsaydam.alarmapp.databinding.FragmentTimerBinding
-import com.umutsaydam.alarmapp.helpers.CustomCountdownTimer
-import com.umutsaydam.alarmapp.helpers.IAlarmNotification
-import com.umutsaydam.alarmapp.helpers.ICustomCountdownTimer
-import com.umutsaydam.alarmapp.helpers.ITimerManager
-import com.umutsaydam.alarmapp.services.AlarmNotificationService
-import java.text.DecimalFormat
-import kotlin.math.roundToInt
+import com.umutsaydam.alarmapp.services.TimerService
 
 class TimerFragment : Fragment() {
     private var _binding: FragmentTimerBinding? = null
     private val binding get() = _binding!!
-    private lateinit var customCountdownTimer: ICustomCountdownTimer
-    private lateinit var timerManager: ITimerManager
     private var currHour = 0
     private var currMinute = 0
+    private var isTimerEnable = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -35,19 +30,69 @@ class TimerFragment : Fragment() {
         _binding = FragmentTimerBinding.inflate(inflater, container, false)
 
         initTimePickerUI()
+        initTimeControllerUI()
         initOnBackPressed()
         return binding.root
     }
 
-    private fun initOnBackPressed() {
-        requireActivity().onBackPressedDispatcher.addCallback(
-            this,
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    customCountdownTimer.destroyTimer()
-                    findNavController().navigateUp()
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == "TIMER_TICK") {
+                val timeLeftFormatted = intent.getStringExtra("timeLeftFormatted")
+                val timeLeft = intent.getIntExtra("timeLeftFormatted", 0)
+                Log.d("R/T", "Time left: $timeLeftFormatted seconds *************************")
+                binding.tvTimer.text = timeLeftFormatted
+                updateProgressBar(timeLeft)
+
+                if (!isTimerEnable) {
+                    binding.timePickerTimer.visibility = View.GONE
+                    binding.llTimerController.visibility = View.VISIBLE
+                    binding.tvTimer.visibility = View.VISIBLE
+                    binding.btnStart.visibility = View.GONE
                 }
-            })
+                isTimerEnable = true
+
+                if (timeLeft == 0) {
+                    binding.timePickerTimer.visibility = View.VISIBLE
+                    binding.llTimerController.visibility = View.GONE
+                    binding.tvTimer.visibility = View.GONE
+                    binding.btnStart.visibility = View.VISIBLE
+                    isTimerEnable = false
+                }
+            }
+        }
+    }
+
+    private fun initTimeControllerUI() {
+        binding.btnStart.setOnClickListener {
+            binding.timePickerTimer.visibility = View.GONE
+
+            val serviceIntent = Intent(requireContext(), TimerService::class.java)
+            serviceIntent.putExtra("hour", currHour)
+            serviceIntent.putExtra("minute", currMinute)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                requireContext().startForegroundService(serviceIntent)
+            } else {
+                requireContext().startService(serviceIntent)
+            }
+            initCustomCountdownTimer()
+
+        }
+
+        binding.btnCancel.setOnClickListener {
+            binding.timePickerTimer.visibility = View.VISIBLE
+            binding.tvTimer.visibility = View.GONE
+            binding.llTimerController.visibility = View.GONE
+            binding.btnStart.visibility = View.VISIBLE
+        }
+
+        binding.btnPause.setOnClickListener {
+            togglePauseResumeButtons(visibilityPause = View.GONE, visibilityResume = View.VISIBLE)
+        }
+
+        binding.btnResume.setOnClickListener {
+            togglePauseResumeButtons(visibilityPause = View.VISIBLE, visibilityResume = View.GONE)
+        }
     }
 
     private fun initTimePickerUI() {
@@ -59,57 +104,20 @@ class TimerFragment : Fragment() {
             currMinute = minute
             binding.btnStart.isEnabled = hour > 0 || minute > 0
         }
-        binding.btnStart.setOnClickListener {
-            binding.timePickerTimer.visibility = View.GONE
-            binding.tvTimer.visibility = View.VISIBLE
-            initCustomCountdownTimer()
-        }
     }
 
     private fun initCustomCountdownTimer() {
-        binding.btnStart.visibility = View.GONE
-        binding.llTimerController.visibility = View.VISIBLE
-
         val countdownTime = (currHour * 60 * 60) + (currMinute * 60)
         val clockTime = (countdownTime * 1000).toLong()
         val progressTime = (clockTime / 1000).toFloat()
-        var secondsLeft = 0
-        customCountdownTimer = CustomCountdownTimer(clockTime, 1000)
-
-        customCountdownTimer.onTick = { millisUntilFinished ->
-            val second = (millisUntilFinished / 1000.0f).roundToInt()
-            if (second != secondsLeft) {
-                secondsLeft = second
-
-                timerFormat(secondsLeft, binding.tvTimer, binding.pbTimer)
-            }
-        }
-
-        customCountdownTimer.onFinish = {
-            timerFormat(0, binding.tvTimer, binding.pbTimer)
-        }
         binding.pbTimer.max = progressTime.toInt()
         binding.pbTimer.progress = progressTime.toInt()
+    }
 
-        customCountdownTimer.startTimer()
-
-        binding.btnCancel.setOnClickListener {
-            customCountdownTimer.destroyTimer()
-            binding.timePickerTimer.visibility = View.VISIBLE
-            binding.tvTimer.visibility = View.GONE
-            binding.llTimerController.visibility = View.GONE
-            binding.btnStart.visibility = View.VISIBLE
-        }
-
-        binding.btnPause.setOnClickListener {
-            togglePauseResumeButtons(visibilityPause = View.GONE, visibilityResume = View.VISIBLE)
-            customCountdownTimer.pauseTimer()
-        }
-
-        binding.btnResume.setOnClickListener {
-            togglePauseResumeButtons(visibilityPause = View.VISIBLE, visibilityResume = View.GONE)
-            customCountdownTimer.resumeTimer()
-        }
+    private fun updateProgressBar(seconds: Int) {
+        val clockTime = (seconds * 1000).toLong()
+        val progressTime = (clockTime / 1000).toFloat()
+        binding.pbTimer.progress = progressTime.toInt()
     }
 
     private fun togglePauseResumeButtons(visibilityPause: Int, visibilityResume: Int) {
@@ -117,43 +125,28 @@ class TimerFragment : Fragment() {
         binding.btnResume.visibility = visibilityResume
     }
 
-    private fun timerFormat(secondsLeft: Int, tvTimer: TextView, pbTimer: ProgressBar) {
-        pbTimer.progress = secondsLeft
-        val decimalFormat = DecimalFormat("00")
-        val hour = secondsLeft / 3600
-        val min = (secondsLeft % 3600) / 60
-        val seconds = secondsLeft % 60
-        val formattedTime =
-            "${decimalFormat.format(hour)}:${decimalFormat.format(min)}:${
-                decimalFormat.format(
-                    seconds
-                )
-            }"
-        tvTimer.text = formattedTime
+    private fun initOnBackPressed() {
+        requireActivity().onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    findNavController().navigateUp()
+                }
+            })
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("R/D", "onStart")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("R/D", "onPause")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val serviceIntent = Intent(context, AlarmNotificationService::class.java)
-        context!!.stopService(serviceIntent)
-        Log.d("R/D", "onResume")
+        // BroadcastReceiver'ı kaydedin
+        requireContext().registerReceiver(broadcastReceiver, IntentFilter("TIMER_TICK"))
+        Log.d("R/T", "Broadcast basladi")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("R/D", "onStop")
-        val serviceIntent = Intent(context, AlarmNotificationService::class.java)
-        ContextCompat.startForegroundService(context!!, serviceIntent)
+        // BroadcastReceiver'ı kaldırın
+        requireContext().unregisterReceiver(broadcastReceiver)
+        Log.d("R/T", "Broadcast bitti")
     }
 
     override fun onDestroy() {
